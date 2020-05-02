c6af725927382ba158639de7b8435f46e63ee707
Jake Charron <jcharron0306@gmail.com>
2020-05-02 18:18:52 -0400

    Working .xyz Gen

--- Main.java
+++ Main.java
@@ -1,57 +1,92 @@
-// import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.depict.DepictionGenerator;
-import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.modeling.builder3d.ModelBuilder3D;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
 
 import java.awt.*;
-import java.io.IOException;
+import java.io.FileWriter; 
 
 public class Main {
+    
+    public static IAtom[] atoms;
 
     public static void main(String[] args) throws Exception {
-        
+
         String smiles = "C(=O)(Cl)Cl";
 
-        boolean return2d = true;
+        boolean return2d = false;
 
-        IChemObjectBuilder bldr   = SilentChemObjectBuilder.getInstance();
-
-        SmilesParser smipar = new SmilesParser(bldr);
-
-        IAtomContainer mol = smipar.parseSmiles(smiles);
-
-
-        mol.setTitle(smiles);
-        if(return2d){
-            return2dImg("~/Desktop/", ".png", mol);
-        }else{
-            // return3d
-        }
+        dipiction(smiles, "~/Desktop/", ".png", return2d);
 
         System.out.println("finished");
 
     }
 
 
-    public static void return2dImg(String path, String imgFormat, IAtomContainer molecule) throws CDKException, IOException {
-        
-        DepictionGenerator dptgen = new DepictionGenerator();
+    public static void dipiction(String smiles, String path, String imgFormat, boolean is2d) throws Exception{
+        if (is2d){
 
-        dptgen.withSize(200, 250)
-                .withMolTitle()
-                .withTitleColor(Color.DARK_GRAY);
+            IChemObjectBuilder bldr = SilentChemObjectBuilder.getInstance();
 
-        dptgen.depict(molecule)
-                .writeTo(path + molecule.getTitle() + imgFormat);
+            SmilesParser smipar = new SmilesParser(bldr);
 
+            IAtomContainer mol = smipar.parseSmiles(smiles);
+
+            mol.setTitle(smiles);
+
+            DepictionGenerator dptgen = new DepictionGenerator();
+
+            dptgen.withSize(200, 250).withMolTitle().withTitleColor(Color.DARK_GRAY);
+
+            dptgen.depict(mol).writeTo(path + mol.getTitle() + imgFormat);
+            
+        } else {
+
+            IChemObjectBuilder bldr = SilentChemObjectBuilder.getInstance();
+
+            SmilesParser smipar = new SmilesParser(bldr);
+
+            IAtomContainer mol = smipar.parseSmiles(smiles);
+
+            ModelBuilder3D mb3d = ModelBuilder3D.getInstance(bldr);
+
+            IAtomContainer coords = mb3d.generate3DCoordinates(mol, false);
+
+            try{
+                int totalAtoms = 0;
+                for (IAtom atom : coords.atoms()) {
+                    totalAtoms++;
+                }
+
+                // init atom array
+                atoms = new IAtom[totalAtoms];
+
+                // creating atom array
+                int curAtomIndex = 0;
+                for (IAtom atom : coords.atoms()) {
+                    atoms[curAtomIndex] = atom;
+                    curAtomIndex++;
+                }
+
+            } catch (Exception e) {}
+
+            FileWriter writer = new FileWriter("temp.xyz");
+
+            writer.write(smiles + "\n");
+            writer.write("\n");
+
+            for(int i = 0; i < atoms.length; i++){
+                writer.write(atoms[i].getSymbol() + "\t" + atoms[i].getPoint3d().x  + "\t" + atoms[i].getPoint3d().y + "\t" + atoms[i].getPoint3d().z + "\n");
+            }
+
+            writer.close();
+
+            
+        }
     }
 
-    // public static void return3dImg(String smiles, String path, String imgFormat){
 
-
-
-    
-}
+} 
